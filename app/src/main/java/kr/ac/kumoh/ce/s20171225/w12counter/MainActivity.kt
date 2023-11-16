3e69f10f214111cfbbf44198c9d8f317400027eb
hoon-hoon <kc0393@naver.com>
2023-11-16 14:07:04 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20171225/w12counter/MainActivity.kt
@@ -0,0 +1,114 @@
+package kr.ac.kumoh.ce.s20171225.w12counter
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.ac.kumoh.ce.s20171225.w12counter.ui.theme.W12CounterTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    //Clicker()
+                    Counter()
+                    Counter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    W12CounterTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Clicker() {
+    val (txtString, setTxtString) = remember { mutableStateOf("눌러주세요") }
+
+    Column(modifier = Modifier
+        .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = txtString,
+            fontSize = 70.sp,
+        )
+        Button(modifier = Modifier
+            .fillMaxWidth(),
+            onClick = {
+                setTxtString("눌렸습니다")
+            }) {
+            Text("눌러봐")
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+    val (count, setCount) = rememberSaveable {
+        mutableStateOf(0)
+    };
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count",
+            fontSize = 70.sp,
+        )
+        Row {
+            Button( modifier = Modifier.weight(1f),
+                onClick = {
+                    setCount(count+1)
+                }) {
+                Text("증가")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button( modifier = Modifier.weight(1f),
+                onClick = {
+                    setCount(count-1)
+                }) {
+                Text("감소")
+            }
+        }
+
+    }
+}
